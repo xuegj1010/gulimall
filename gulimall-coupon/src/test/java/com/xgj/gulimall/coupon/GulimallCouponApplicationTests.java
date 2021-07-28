1f9773da4bee1f0c1cd2383461fb6dddbbc464cc
xuegj1010 <869624090@qq.com>
2021-07-28 21:26:07 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/xgj/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgj.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/test/java/com/xgj/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgj.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

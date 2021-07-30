b2f1e95af999e7c89e7b098e5073e0bc146aca51
xuegj1010 <869624090@qq.com>
2021-07-31 00:10:28 +0800

    添加nacos注册服务

--- gulimall-product/src/main/java/com/xgj/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/xgj/gulimall/product/GulimallProductApplication.java
@@ -3,8 +3,10 @@ package com.xgj.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.xgj.gulimall.product.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallProductApplication {
 

863ccbb4843622983eafa149f779ceb4cdbcdb2d
xuegj1010 <869624090@qq.com>
2021-08-05 22:52:16 +0800

    添加Gateway网关

---
+++ gulimall-gateway/src/test/java/com/xgj/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgj.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

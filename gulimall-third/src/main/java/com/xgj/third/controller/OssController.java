23b801472d3c5ccd20efd6189b4ac5e58320f4e8
xuegj1010 <869624090@qq.com>
2021-08-18 22:34:15 +0800

    添加OSS文件上传功能

--- gulimall-third/src/main/java/com/xgj/third/controller/OssController.java
+++ gulimall-third/src/main/java/com/xgj/third/controller/OssController.java
@@ -5,6 +5,7 @@ import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
+import com.xgj.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,7 @@ public class OssController {
     private String accessId;
 
     @RequestMapping("/oss/policy")
-    public Map<String, String> policy() {
+    public R policy() {
 
         //https://gulimall-xgj.oss-cn-shanghai.aliyuncs.com/%E5%A4%B4%E5%83%8F/d100fa5bfd99dd0379d4774895738055.jpg
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
@@ -71,6 +72,6 @@ public class OssController {
         } finally {
             ossClient.shutdown();
         }
-        return respMap;
+        return R.ok().put("data",respMap);
     }
 }
